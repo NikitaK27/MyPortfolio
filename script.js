8376bc2e2404c77793d00227e70a310fe2383dc4
NikitaK27 <80617331+NikitaK27@users.noreply.github.com>
2024-02-07 13:38:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// (()=>{
+//     const hamburgerBtn = document.getElementsByClassName(".hamburger-btn"),
+//     navMenu= document.getElementsByClassName(".nav-menu"),
+//     closeNavBtn= navMenu.getElementsByClassName(".close-nav-menu");
+
+//     hamburgerBtn.addEventListener("click",showNavMenu);
+//     function showNavMenu(){
+//         navMenu.classList.toggle("open");
+//     }
+// })();
+
+
+// (()=>{
+//   const sections =document.getElementsByClassName(".section");
+//   sections.forEach((section)=>{
+//        if(!section.classList.contains("active")){
+//            section.classList.add("hide");
+//        }
+//   })
+// })();
